e7809cb45ad4ba877a75b580a17d3a6c646023a7
Ayan Sengupta <ayan.sengupta27@gmail.com>
2018-04-23 23:52:03 -0400

    home1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { EditListingComponent } from './components/edit-listing/edit-listing.com
 
 const appRoutes: Routes=[
   {path:'', component: HomeComponent},
-  {path:'listings', component: ListingsComponent}
+  {path:'listings', component: ListingsComponent},
+  {path:'add-listing', component: AddListingComponent}
 ]
 
 
